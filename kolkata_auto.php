3765f100a762e306fe519ebccb896ac6139f3c01
Paromiat Roy <paromita.roy@outlook.com>
2021-05-02 15:58:30 +0100

    img

--- kolkata_auto.php
+++ kolkata_auto.php
@@ -59,8 +59,9 @@
             echo "<br>";
             echo 'Category is ',$category;
             echo "<br>";
-            echo <img src="/uploadKolkata/1619966361-Oxygen+Screenshot 2021-05-02 125754.jpg">;
-            echo '<img src="', $dir, $file, '" alt="', $file, '" /,width="380" height="350 /n">';
+            echo '<img src="uploadKolkata/1619966361-Oxygen+Screenshot 2021-05-02 125754.jpg">';
+
+           # echo '<img src="', $dir, $file, '" alt="', $file, '" /,width="380" height="350 /n">';
             echo "<br>";
             echo "<br>";
             echo "<br>";
